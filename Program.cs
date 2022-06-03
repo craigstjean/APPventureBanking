94d36a8fabbda85dfaedc6ed61fa9d8d0c1b07d9
Craig St. Jean <craigstjean@gmail.com>
2022-06-02 21:58:02 -0400

    Enable enums as strings

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using APPventureBanking.Filters;
 using APPventureBanking.Models;
 using APPventureBanking.Services;
@@ -8,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BankContext>();
 builder.Services.AddTransient<AccountService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
