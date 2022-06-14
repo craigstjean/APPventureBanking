ef86d20810163f8c90ad96300523c08a2eb6efff
Craig St. Jean <craigstjean@gmail.com>
2022-06-14 16:02:42 -0400

    Get Party should only return the current user's party information

--- Controllers/PartyController.cs
+++ Controllers/PartyController.cs
@@ -17,64 +17,18 @@ public class PartyController : ControllerBase
     }
     
     [HttpGet]
-    public IEnumerable<Party> Get()
+    [ProducesResponseType(typeof(Party), 200)]
+    public IActionResult Get()
     {
-        return _context.Parties;
-    }
-    
-    [HttpGet("{id}")]
-    public IActionResult Get(int id)
-    {
-        var party = _context.Parties.Find(id);
-        if (party == null)
+        Request.Headers.TryGetValue("Authorization", out var authorizationHeader);
+        var identity = authorizationHeader.Count == 0 ? null
+            : _context.Identities.Find(int.Parse(authorizationHeader[0].Split(" ")[1]));
+        
+        if (identity == null)
         {
-            return NotFound();
-        }
-        return Ok(party);
-    }
-    
-    [HttpPost]
-    public IActionResult Post([FromBody] Party? party)
-    {
-        if (party == null)
-        {
-            return BadRequest();
+            return Unauthorized();
         }
         
-        _context.Parties.Add(party);
-        _context.SaveChanges();
-        
-        return CreatedAtAction("Get", new { id = party.PartyId }, party);
-    }
-    
-    [HttpPut("{id}")]
-    public IActionResult Put(int id, [FromBody] Party party)
-    {
-        var partyToUpdate = _context.Parties.Find(id);
-        if (partyToUpdate == null)
-        {
-            return NotFound();
-        }
-        partyToUpdate.Type = party.Type;
-        partyToUpdate.EntityName = party.EntityName;
-        partyToUpdate.FirstName = party.FirstName;
-        partyToUpdate.LastName = party.LastName;
-        partyToUpdate.PrimaryEmailAddress = party.PrimaryEmailAddress;
-        partyToUpdate.MailingAddress = party.MailingAddress;
-        _context.SaveChanges();
-        return NoContent();
-    }
-    
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-    {
-        var partyToDelete = _context.Parties.Find(id);
-        if (partyToDelete == null)
-        {
-            return NotFound();
-        }
-        _context.Parties.Remove(partyToDelete);
-        _context.SaveChanges();
-        return NoContent();
+        return Ok(_context.Parties.Find(identity.IdentityId));
     }
 }
