c84065a27448da4786f32d3dbef855a7b18f4279
Craig St. Jean <craigstjean@gmail.com>
2022-06-17 11:10:38 -0400

    Set various request inputs as optional

--- Controllers/TransferObjects/Admin/CreateBillingPayeeRequest.cs
+++ Controllers/TransferObjects/Admin/CreateBillingPayeeRequest.cs
@@ -5,7 +5,7 @@ public class CreateBillingPayeeRequest
     public string EntityName { get; set; }
     public string PrimaryEmailAddress { get; set; }
     public string AddressLine1 { get; set; }
-    public string AddressLine2 { get; set; }
+    public string? AddressLine2 { get; set; }
     public string City { get; set; }
     public string StateCode { get; set; }
     public string PostalCode { get; set; }
--- Controllers/TransferObjects/Admin/CreateIdentityRequest.cs
+++ Controllers/TransferObjects/Admin/CreateIdentityRequest.cs
@@ -5,9 +5,9 @@ namespace APPventureBanking.Controllers.TransferObjects.Admin;
 public class CreateIdentityRequest
 {
     public PartyType Type { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string EntityName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? EntityName { get; set; }
     public string PrimaryEmailAddress { get; set; }
     public CreateAddressRequest MailingAddress { get; set; }
 }
